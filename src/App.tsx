3e813dafa9caecaa45ffb20e36760498f0785b44
maximoaf <maximoscooby@gmail.com>
2025-04-29 02:22:46 -0300

    Footer / v0.4

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Home } from "./apps/landing/pages/Home";
-import { Login } from "./apps/landing/components/ingreso/Login";
 import { Ingreso } from "./apps/landing/pages/Ingreso";
 
 export const App = () => {
