2f5ded5d7f0af2b1bf6ce790996c443c3bfd0592
maximoaf <maximoscooby@gmail.com>
2025-04-30 10:56:34 -0300

    Error page / v0.06

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import { Home } from "./apps/landing/pages/Home";
 import { Ingreso } from "./apps/landing/pages/Ingreso";
+import { ErrorPage } from "./apps/landing/pages/ErrorPage";
 
 export const App = () => {
-  return <Ingreso />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/ingreso" element={<Ingreso />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+  );
 };
