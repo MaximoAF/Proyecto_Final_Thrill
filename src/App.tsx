b486d4d9903f8b98608257221962a09c11e4bdd5
maximoaf <maximoscooby@gmail.com>
2025-05-12 17:25:33 -0300

    Cart / v0.09

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "./apps/landing/pages/Home";
 import { Ingreso } from "./apps/landing/pages/Ingreso";
 import { ErrorPage } from "./apps/landing/pages/ErrorPage";
+import { Cart } from "./apps/landing/pages/Cart";
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/ingreso" element={<Ingreso />} />
+      <Route path="/carrito" element={<Cart />} />
       <Route path="*" element={<ErrorPage />} />
     </Routes>
   );
