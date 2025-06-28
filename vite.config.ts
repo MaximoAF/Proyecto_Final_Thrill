b4fda4bb885ff4fd60f217962a0f639d9693324b
maximoaf <maximoscooby@gmail.com>
2025-06-28 02:17:38 -0300

    Error netlify / v0.51

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // <- permite acceder desde la red
-    strictPort: true,
-    port: 5173,
-    allowedHosts: ['.ngrok-free.app'] // <- permite ngrok
-  }
+  base: "/", // <- necesario para rutas relativas en producción
 })
